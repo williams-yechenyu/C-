9710a525a50ed404216e29bd53f4c8e6700bcd6b
叶晨宇 <81900456+williams-yechenyu@users.noreply.github.com>
2022-07-15 17:47:01 +0800

    Create point.cpp

---
+++ 进阶知识部分/封装/设计案例2/point.cpp
@@ -0,0 +1,22 @@
+#include"point.h"
+
+//设置x
+void Point::setX(int x)
+{
+	m_X = x;
+}
+//获取x
+int Point::getX()
+{
+	return m_X;
+}
+//设置y
+void Point::setY(int y)
+{
+	m_Y = y;
+}
+//获取y
+int Point::getY()
+{
+	return m_Y;
+}
