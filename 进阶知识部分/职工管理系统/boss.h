f9caf573f0cba9f540acfb6440ad2e527ad153e8
叶晨宇 <81900456+williams-yechenyu@users.noreply.github.com>
2022-09-23 23:46:00 +0800

    Create boss.h

---
+++ 进阶知识部分/职工管理系统/boss.h
@@ -0,0 +1,20 @@
+#ifndef STAFFSYSTEM_BOSS_H
+#define STAFFSYSTEM_BOSS_H
+#endif //STAFFSYSTEM_BOSS_H
+
+#pragma once
+#include<iostream>
+using namespace std;
+#include"worker.h"
+
+//老板类
+class Boss:public Worker{
+public:
+    Boss(int id,string name,int dId);
+
+    //显示个人信息
+    virtual void showInfo();
+
+    //获取职工岗位名称
+    virtual string getDeptName();
+};
