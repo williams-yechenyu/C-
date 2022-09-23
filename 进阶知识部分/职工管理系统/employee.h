bbe683b64efbdaa9267989d205a9f4c0beebd56b
叶晨宇 <81900456+williams-yechenyu@users.noreply.github.com>
2022-09-23 23:47:03 +0800

    Create employee.h

---
+++ 进阶知识部分/职工管理系统/employee.h
@@ -0,0 +1,21 @@
+#ifndef STAFFSYSTEM_EMPLOYEE_H
+#define STAFFSYSTEM_EMPLOYEE_H
+#endif //STAFFSYSTEM_EMPLOYEE_H
+
+#pragma once
+#include<iostream>
+using namespace std;
+#include"worker.h"
+
+//员工类
+class Employee:public Worker{
+public:
+    //构造函数
+    Employee(int id,string name,int dId);
+
+    //显示个人信息
+    virtual void showInfo();
+
+    //获取职工岗位名称
+    virtual string getDeptName();
+};
