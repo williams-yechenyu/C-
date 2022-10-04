30eb38e931b2fba33882ae53efeb91121d54c02f
叶晨宇 <81900456+williams-yechenyu@users.noreply.github.com>
2022-10-04 20:33:49 +0800

    Create person.cpp

---
+++ 高阶知识部分/类模板/类模板分文件编写/person.cpp
@@ -0,0 +1,13 @@
+//#include"person.h"
+//template<class T1, class T2>
+//Person<T1, T2>::Person(T1 name, T2 age)
+//{
+//	this->m_name = name;
+//	this->m_age = age;
+//}
+//
+//template<class T1, class T2>
+//void Person<T1, T2>::showPerson()
+//{
+//	cout << "姓名：" << this->m_name << "年龄：" << this->m_age << endl;
+//}
