8bff1e8f40e250b2c28013f745d9a1a10aac7bd2
叶晨宇 <81900456+williams-yechenyu@users.noreply.github.com>
2022-11-07 10:20:48 +0800

    Create computerRoom.h

---
+++ 高阶知识部分/机房预约系统/computerRoom.h
@@ -0,0 +1,18 @@
+#ifndef MACHINE_ROOM_RESERVATION_SYSTEM_COMPUTERROOM_H
+#define MACHINE_ROOM_RESERVATION_SYSTEM_COMPUTERROOM_H
+
+#endif //MACHINE_ROOM_RESERVATION_SYSTEM_COMPUTERROOM_H
+
+#pragma once
+#include <iostream>
+using namespace std;
+
+//机房类
+class ComputerRoom
+{
+public:
+
+    int m_ComId; //机房Id号
+
+    int m_MaxNum; //机房最大容量
+};
