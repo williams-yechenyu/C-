db8b7ec7ef12d97bac3a6b54a18b0792a149ba45
叶晨宇 <81900456+williams-yechenyu@users.noreply.github.com>
2022-11-07 10:24:48 +0800

    Create orderFile.h

---
+++ 高阶知识部分/机房预约系统/orderFile.h
@@ -0,0 +1,30 @@
+#ifndef MACHINE_ROOM_RESERVATION_SYSTEM_ORDERFILE_H
+#define MACHINE_ROOM_RESERVATION_SYSTEM_ORDERFILE_H
+
+#endif //MACHINE_ROOM_RESERVATION_SYSTEM_ORDERFILE_H
+
+#pragma once
+#include <iostream>
+using namespace std;
+#include "globalFile.h"
+#include <fstream>
+#include <map>
+#include <string>
+
+class OrderFile
+{
+public:
+
+    //构造函数
+    OrderFile();
+
+    //更新预约记录
+    void updateOrder();
+
+    //记录预约条数
+    int m_Size;
+
+    //记录所有预约信息的容器  key记录条数   value 具体记录键值对信息
+    map<int, map<string, string>> m_orderData;
+
+};
