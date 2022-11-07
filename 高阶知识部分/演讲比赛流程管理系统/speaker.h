50437ec428fb10cb7c9677152c82f4e62acbf356
叶晨宇 <81900456+williams-yechenyu@users.noreply.github.com>
2022-11-07 10:40:04 +0800

    Create speaker.h

---
+++ 高阶知识部分/演讲比赛流程管理系统/speaker.h
@@ -0,0 +1,16 @@
+#ifndef SPEECH_CONTEST_PROCESS_MANAGEMENT_SYSTEM_SPEAKER_H
+#define SPEECH_CONTEST_PROCESS_MANAGEMENT_SYSTEM_SPEAKER_H
+#endif //SPEECH_CONTEST_PROCESS_MANAGEMENT_SYSTEM_SPEAKER_H
+
+#pragma once
+#include <iostream>
+using namespace std;
+
+//选手类
+class Speaker
+{
+public:
+
+    string m_Name;    //姓名
+    double m_Score[2]; //分数  最多有两轮得分
+};
