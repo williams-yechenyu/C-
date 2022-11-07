b9f791b7b121cd2dd77e8a03ba395a297fd995e2
叶晨宇 <81900456+williams-yechenyu@users.noreply.github.com>
2022-11-07 10:21:48 +0800

    Create Identity.h

---
+++ 高阶知识部分/机房预约系统/Identity.h
@@ -0,0 +1,22 @@
+#ifndef MACHINE_ROOM_RESERVATION_SYSTEM_IDENTITY_H
+#define MACHINE_ROOM_RESERVATION_SYSTEM_IDENTITY_H
+
+#endif //MACHINE_ROOM_RESERVATION_SYSTEM_IDENTITY_H
+
+#pragma once //防止头文件重复包含
+#include <iostream>
+using namespace std;
+
+//身份抽象基类
+class Identity
+{
+public:
+
+    //操作菜单   纯虚函数
+    virtual void operMenu() = 0;
+
+    //用户名
+    string m_Name;
+    //密码
+    string m_Pwd;
+};
