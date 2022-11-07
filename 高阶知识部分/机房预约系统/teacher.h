430a64efe7b0a967d5ef56c3b73d210cc4c3eafc
叶晨宇 <81900456+williams-yechenyu@users.noreply.github.com>
2022-11-07 10:26:53 +0800

    Create teacher.h

---
+++ 高阶知识部分/机房预约系统/teacher.h
@@ -0,0 +1,35 @@
+#ifndef MACHINE_ROOM_RESERVATION_SYSTEM_TEACHER_H
+#define MACHINE_ROOM_RESERVATION_SYSTEM_TEACHER_H
+
+#endif //MACHINE_ROOM_RESERVATION_SYSTEM_TEACHER_H
+
+#pragma once
+#include <iostream>
+using namespace std;
+#include "Identity.h"
+#include <string>
+#include "orderFile.h"
+#include <vector>
+
+//教师类设计
+class Teacher :public Identity
+{
+public:
+    //默认构造
+    Teacher();
+
+    //有参构造
+    Teacher(int empId, string name, string pwd);
+
+    //菜单界面
+    virtual void operMenu();
+
+    //查看所有预约
+    void showAllOrder();
+
+    //审核预约
+    void validOrder();
+
+    //职工号
+    int m_EmpId;
+};
